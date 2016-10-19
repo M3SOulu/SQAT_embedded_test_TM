32722ac8b761156744c1807e08ed77fa8ba09db8
anrehman <anrehman@stupid-pc0184.stnet.local>
2016-10-19 12:06:16 +0300

    Temperature Monitoring - Aniqa Rehman

--- src/display.cpp
+++ src/display.cpp
@@ -10,36 +10,28 @@
 #include "ht16k33.h"
 #include "display.h"
 
-int disp_off()
-{
+int disp_off() {
 	const int addr = HW_I2C_ADDR_HT16K33;
 	char data[10];
-	int rc=0;
+	int rc = 0;
 
 	data[0] = HT16K33_CMD_OSCILLATOR_OFF;
-	data[1]=0;
+	data[1] = 0;
 
-	rc = i2c_write( addr,data,1 );
+	rc = i2c_write(addr, data, 1);
 	return rc;
 }
 
-static char disp_msg_data[10]={
-		0,0,
-		1,0,
-		2,0,
-		3,0,
-		4,0,
-};
+static char disp_msg_data[10] = { 0, 0, 1, 0, 2, 0, 3, 0, 4, 0, };
 
 static int disp_last_message = DISP_MSG_FIRST;
 
 //
 // set all display data to either "all on" or "all off"
 //
-static void disp_set_all(int alloff)
-{
+static void disp_set_all(int alloff) {
 	char value = 0;
-	switch( alloff ){
+	switch (alloff) {
 	case DISP_SHOW_NONE:
 		value = SEGMENTS_NONE;
 		break;
@@ -60,40 +52,57 @@ static void disp_set_all(int alloff)
 // reset display data to all off/on
 // clear display message value
 //
-void disp_reset(int alloff)
-{
-	disp_set_all( alloff );
+void disp_reset(int alloff) {
+	disp_set_all(alloff);
 	disp_last_message = DISP_MSG_FIRST;
 }
 
-//
-// power on display, set the clocking and initial data
-// - data is all off/on
-//
-int disp_on(int alloff)
-{
+void disp_set_segment_up() {
+
+}
+
+void disp_set_segment_down() {
+
+}
+
+void disp_set_segment_same() {
+
+}
+
+
+void convertMessageToDispMsgData(display_message_t message){
+	if(message == DISP_MSG_UP)
+		disp_set_segment_up();
+	else if (message == DISP_MSG_DOWN)
+		disp_set_segment_down();
+	else
+		disp_set_segment_same();
+}
+
+
+int disp_on(int alloff) {
 	const int addr = HW_I2C_ADDR_HT16K33;
 	char disp_cmd_data[10];
-	int rc=0;
+	int rc = 0;
 
 	disp_cmd_data[0] = HT16K33_CMD_OSCILLATOR_ON;
-	disp_cmd_data[1]=0;
+	disp_cmd_data[1] = 0;
 
-	rc = i2c_write( addr,disp_cmd_data,1 );
-	if ( 0 <= rc ){
+	rc = i2c_write(addr, disp_cmd_data, 1);
+	if (0 <= rc) {
 		disp_cmd_data[0] = HT16K33_BLINK_CMD | 0x01;
-		rc = i2c_write( addr,disp_cmd_data,1 );
+		rc = i2c_write(addr, disp_cmd_data, 1);
 	} else {
 		return rc;
 	}
-	if ( 0 <= rc ){
+	if (0 <= rc) {
 		disp_cmd_data[0] = HT16K33_CMD_BRIGHTNESS | 0x08;
-		rc = i2c_write( addr,disp_cmd_data,1 );
+		rc = i2c_write(addr, disp_cmd_data, 1);
 	} else {
 		return rc;
 	}
-	disp_set_all( alloff );
-	rc = i2c_write( addr,disp_msg_data,10 );
+	disp_set_all(alloff);
+	rc = i2c_write(addr, disp_msg_data, 10);
 
 	return rc;
 }
@@ -104,9 +113,14 @@ int disp_on(int alloff)
  *
  ************************************************************************/
 
-void rotate_message_left()
-{
+void rotate_message_left() {
 
+	int temp = disp_msg_data[1];
+
+	disp_msg_data[1] = disp_msg_data[3];
+	disp_msg_data[3] = disp_msg_data[7];
+	disp_msg_data[7] = disp_msg_data[9];
+	disp_msg_data[9] = temp;
 }
 
 //
@@ -116,8 +130,9 @@ void rotate_message_left()
 //   - DISP_MSG_UP
 // if message is same as previous then rotate left
 //
-int disp_show_message(display_message_t message)
-{
+int disp_show_message(display_message_t message) {
 	const int addr = HW_I2C_ADDR_HT16K33;
-	return i2c_write( addr, disp_msg_data,10 );
+	convertMessageToDispMsgData(message);
+	return i2c_write(addr, disp_msg_data, 10);
 }
+
--- src/tm1.cpp
+++ src/tm1.cpp
@@ -16,53 +16,82 @@
 // throw them away if not
 
 #define TEMP_BUFFER 8
-int temperatures[TEMP_BUFFER] = {
-		0,0,0,0,
-		0,0,0,0
-};
+int temperatures[TEMP_BUFFER] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
 //
 // return the Nth valuke in the temperatures array
 //
-int tm_get_temperature(int n)
-{
-	return -1;
+int tm_get_temperature(int n) {
+	return temperatures[n];
 }
 
-int temp_index=0;
+int temp_index = 0;
 
-int temp_sum=0;
+int temp_sum = 0;
 int temp_old_sum = 0;
 
-int temp_prev_average=0;
-int temp_current_average=0;
+int temp_prev_average = 0;
+int temp_current_average = 0;
 
 display_message_t temp_trend = DISP_MSG_SAME;
 
 //
 // reset all temperature data (above, including array)
 //
-void tm_reset_data()
-{
+void tm_reset_data() {
+	temp_index = 0;
+	temperatures[0] = 0;
+	temperatures[1] = 0;
+	temperatures[2] = 0;
+	temperatures[3] = 0;
+	temperatures[4] = 0;
+	temperatures[5] = 0;
+	temperatures[6] = 0;
+	temperatures[7] = 0;
 }
 
 //
-// update calculations
-//
-void tm_update_average(int temp)
-{
-}
-//
 // return the average
 //
-int tm_get_average()
-{
-	return -1;
+int tm_get_average() {
+	temp_prev_average = temp_current_average;
+
+	temp_current_average = 0;
+
+	temp_current_average = (temperatures[0] + temperatures[1] + temperatures[2]
+			+ temperatures[3] + temperatures[4] + temperatures[5]
+			+ temperatures[6] + temperatures[7]) / 8;
+	return temp_current_average;
 }
+
+void remove_first_temp() {
+	int i = 0;
+	for (i = 0; i < 7; i++) {
+		temperatures[i] = temperatures[i + 1];
+	}
+	temperatures[i] = 0;
+}
+//
+// update calculations
+//
+void tm_update_average(int temp) {
+
+	if (temp_index == 8) {
+		remove_first_temp();
+		temp_index--;
+	}
+	temperatures[temp_index] = temp;
+	temp_index++;
+	tm_get_average();
+}
+
 //
 // get the current trend value
 //
-display_message_t tm_get_trend()
-{
+display_message_t tm_get_trend() {
+	if(temp_current_average < temp_prev_average)
+		return DISP_MSG_DOWN;
+	else if (temp_current_average > temp_prev_average)
+		return DISP_MSG_UP;
 	return DISP_MSG_SAME; // default
 }
