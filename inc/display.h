c36718cfad41f03a286d082ce576ad9d10d98adf
root <root@stupid-pc0184.stnet.local>
2016-10-19 12:10:15 +0300

    Juha E. E. Pätsi 20750 TemperatureMonitoring only "Same" test completed, leaving at 12:10

--- inc/display.h
+++ inc/display.h
@@ -15,11 +15,23 @@
 #define DISP_SHOW_ALL   1
 #define DISP_SHOW_NONE  0
 
-#define SEGMENT_NONE 0
+#define SEGMENT_NONE 0x00
 #define SEGMENT_ALL  0xFF
 
-#define SEGMENT_0    63
-#define SEGMENT_1     6
+#define SEGMENT_U    0x1C
+#define SEGMENT_P    0x73
+#define SEGMENT_A    0x77
+#define SEGMENT_E    0x79
+#define SEGMENT_D    0x5E
+#define SEGMENT_N    0x54
+#define SEGMENT_M    0x37
+#define SEGMENT_O    0x5C
+#define SEGMENT_P    0x73
+#define SEGMENT_S    0x6D
+#define SEGMENT_W    0x7E
+
+#define SEGMENT_0    0x3F
+#define SEGMENT_1    0X06
 
 int disp_off();
 int disp_on(int alloff);
@@ -30,7 +42,7 @@ typedef enum display_message_e {
 	DISP_MSG_DOWN = -1,
 	DISP_MSG_SAME = 0,
 	DISP_MSG_UP   = 1,
-	DISP_MSG_LAST
+	DISP_MSG_LAST = 2
 } display_message_t;
 
 int disp_show_message( display_message_t message );
